accab6772c6c447b8317f1d054fdcb0dc4350a46
Andrea Torelli <gioran326@protonmail.com>
2023-02-23 20:41:55 +0100

    Costo ed offerta Finiti

--- js/main.js
+++ js/main.js
@@ -1,46 +1,32 @@
+const km = kmDaPercorrere.value;
 const genera = document.querySelector('#genera');
+const prezzoPieno = (km * 0.21);
+const sconto20 = (prezzoPieno / 100 * 20); //per sapere di quanto verrà scontato il prezzo
+const sconto40 = (prezzoPieno / 100 * 40); //per sapere di quanto verrà scontato il prezzo
+let prezzoFinale;
+const etaDom = document.getElementById('eta').value;
 
 genera.addEventListener('click', 
     function () {
-        const messaggioDom = document.getElementById('messaggio');
-        const testoDelMessaggio = messaggioDom.value;
-        alert(testoDelMessaggio);
+      const nomeDom = document.getElementById('nome').value;
+      document.getElementById("rNome").innerHTML = nomeDom;
 
-        const casellaSelezioneDom = document.getElementById('casellaSelezione');
-        alert(casellaSelezioneDom.value);
+      if (etaDom == 'Minorenne') {
+         document.getElementById("tipoBiglietto").innerHTML = "Sconto (20%) Minorenne";
+         prezzoFinale = prezzoPieno - sconto20; 
+      } else if (etaDom == 'Over65') {
+         document.getElementById("tipoBiglietto").innerHTML = "Sconto (40%) Over65";
+         prezzoFinale = prezzoPieno - sconto40;
+      } else if (etaDom == 'Maggiorenne') {
+         document.getElementById("tipoBiglietto").innerHTML = "Biglietto Standard";
+         prezzoFinale = prezzoPieno;
+      }
+
+      document.getElementById("costoBiglietto").innerHTML = (prezzoFinale).toFixed(2) + "€";
+
+
     }
 );
-
-
-
-
-/*
-document.getElementById("km").innerHTML = "Km da percorrere: " + km; 
-document.getElementById("eta").innerHTML = "Età: " + eta; 
-
-const prezzoPieno = (km * 0.21);
-document.getElementById("prezzoPieno").innerHTML = "Il prezzo del biglietto è: " + (prezzoPieno).toFixed(2) + "€";
-
-const sconto20 = (prezzoPieno / 100 * 20); //per sapere di quanto verrà scontato il prezzo
-const sconto40 = (prezzoPieno / 100 * 40); //per sapere di quanto verrà scontato il prezzo
-
-let prezzoFinale;
-
-if (eta <= 17) {
-   document.getElementById("sconto").innerHTML = "Visto che sei un* pischell*, ti facciamo uno sconto di: " + (sconto20).toFixed(2) + "€";
-   prezzoFinale = prezzoPieno - sconto20;
-} else if (eta >= 65) {
-   document.getElementById("sconto").innerHTML = "Sconto vecchi di: " + (sconto40).toFixed(2) + "€";
-   prezzoFinale = prezzoPieno - sconto40;
-} else {
-   document.getElementById("sconto").innerHTML = "tè un sfighé...";
-   prezzoFinale = prezzoPieno;
-}
-
-document.getElementById("prezzoFinale").innerHTML = "Paga: " + (prezzoFinale).toFixed(2) + "€";
-
-*/
-
 
 
 console.log("Fine!");
