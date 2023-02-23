630b4b0cc6296cc4fd39edb61bbe3e1836096d6b
Andrea Torelli <gioran326@protonmail.com>
2023-02-23 14:55:16 +0100

    Creazione struttura

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+const km = parseInt(prompt("Quanti Km devi percorrere"));
+const eta = parseInt(prompt("Quanti anni hai?"));
+
+
+
+/*
+document.getElementById("km").innerHTML = "Km da percorrere: " + km; 
+document.getElementById("eta").innerHTML = "Età: " + eta; 
+
+const prezzoPieno = (km * 0.21);
+document.getElementById("prezzoPieno").innerHTML = "Il prezzo del biglietto è: " + (prezzoPieno).toFixed(2) + "€";
+
+const sconto20 = (prezzoPieno / 100 * 20); //per sapere di quanto verrà scontato il prezzo
+const sconto40 = (prezzoPieno / 100 * 40); //per sapere di quanto verrà scontato il prezzo
+
+let prezzoFinale;
+
+if (eta <= 17) {
+   document.getElementById("sconto").innerHTML = "Visto che sei un* pischell*, ti facciamo uno sconto di: " + (sconto20).toFixed(2) + "€";
+   prezzoFinale = prezzoPieno - sconto20;
+} else if (eta >= 65) {
+   document.getElementById("sconto").innerHTML = "Sconto vecchi di: " + (sconto40).toFixed(2) + "€";
+   prezzoFinale = prezzoPieno - sconto40;
+} else {
+   document.getElementById("sconto").innerHTML = "tè un sfighé...";
+   prezzoFinale = prezzoPieno;
+}
+
+document.getElementById("prezzoFinale").innerHTML = "Paga: " + (prezzoFinale).toFixed(2) + "€";
+
+*/
+
+
+
+console.log("Fine!");
